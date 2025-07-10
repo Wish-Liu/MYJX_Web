2f1a8bbed832528c7419b70bd46c9311d4f2abca
Wish <yaoz608970@gmail.com>
2025-07-10 15:05:39 +0800

    优化APP主页面显示

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import legacy from '@vitejs/plugin-legacy'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/MYJX_Web/', // ✅ 关键：必须和你的仓库名保持一致，区分大小写
+  base: '/MYJX_Web/',
   plugins: [vue(), vueDevTools(), legacy({
     targets: ['defaults', 'not IE 11'],
   }),],
